289b46b8999c174a70d06967ad92b1faa59aa30a
Ianfb <124350699+ianfb37@users.noreply.github.com>
2024-10-25 08:37:05 +0200

    Add files via upload

---
+++ Tema3Practica4/Operaciones.php
@@ -0,0 +1,14 @@
+<?php
+function suma($num1,$num2){
+    return $num1+$num2;
+}
+function resta ($num1,$num2){
+    return $num1-$num2;
+}
+function multiplicacion ($num1,$num2){
+    return $num1-$num2;
+}
+function division ($num1,$num2){
+    return $num1/$num2;
+}
+?>

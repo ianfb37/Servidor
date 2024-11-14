2f2fea0cadd660d1f6934e7476b92c5b001608fd
Ianfb <124350699+ianfb37@users.noreply.github.com>
2024-11-14 14:08:06 +0100

    Update Ej2.php

--- Parcial_Ian/Ej2/Ej2.php
+++ Parcial_Ian/Ej2/Ej2.php
@@ -10,14 +10,13 @@
 
     <?php
     session_start();
-    
+    for($i=0;$i<=4;$i++){
+        $ialeatorio[$i]=rand(0,1);
+    }
     ?>
      <form action="contador.php" method="post">
         <input type="submit" name="restar" value="Restar">
-        <input type="text" id="numero" name="numero" value="<?php rand(0,1)?>">
-        <input type="text" id="numero" name="numero" value="<?php rand(0,1)?>">
-        <input type="text" id="numero" name="numero" value="<?php rand(0,1)?>">
-        <input type="text" id="numero" name="numero" value="<?php rand(0,1)?>">
+        
         <input type="submit" name="sumar" value="Sumar">
         <input type="submit" name="enviar" value="Enviar">
 
